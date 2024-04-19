d24f343d94c357c49a2937f80fca11095f202d46
MsEspeon <missingespeon@gmail.com>
2024-04-19 15:16:24 +0800

    Initial commit

---
+++ _site/assets/js/data/swconf.js
@@ -0,0 +1,38 @@
+const swconf = {
+  
+    cacheName: 'chirpy-1713510340',resources: [
+      '/ittousei/assets/css/jekyll-theme-chirpy.css',
+      '/ittousei/',
+      
+        '/ittousei/categories/',
+      
+        '/ittousei/tags/',
+      
+        '/ittousei/archives/',
+      
+        '/ittousei/about/',
+      
+
+      
+      
+        '/ittousei/assets/js/dist/categories.min.js',
+        '/ittousei/assets/js/dist/commons.min.js',
+        '/ittousei/assets/js/dist/home.min.js',
+        '/ittousei/assets/js/dist/misc.min.js',
+        '/ittousei/assets/js/dist/page.min.js',
+        '/ittousei/assets/js/dist/post.min.js'
+    ],allowHosts: [
+      
+          'fonts.googleapis.com',
+          'fonts.gstatic.com',
+          'fonts.googleapis.com',
+          'cdn.jsdelivr.net',
+          'cdnjs.cloudflare.com'
+      
+    ],denyPaths: [
+      
+    ],
+    purge: false
+  
+};
+
