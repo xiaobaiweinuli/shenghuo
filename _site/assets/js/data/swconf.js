244e17ff2a6ca6b0d02d248e9892ceb849c0a1a2
MsEspeon <missingespeon@gmail.com>
2024-04-20 16:12:21 +0800

    Search test

--- _site/assets/js/data/swconf.js
+++ _site/assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1713600442',resources: [
+    cacheName: 'chirpy-1713600708',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
