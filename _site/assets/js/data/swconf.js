4fc1a2e3d84ec9ade960fd7e1337a3d668a81081
MsEspeon <missingespeon@gmail.com>
2024-04-20 20:29:50 +0800

    Treehole test

--- _site/assets/js/data/swconf.js
+++ _site/assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1713602069',resources: [
+    cacheName: 'chirpy-1713616181',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
